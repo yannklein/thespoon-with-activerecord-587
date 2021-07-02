80cb50e2286ee5ee8a7f602219c93c7a213ecfe2
Yann Klein <yann.klein@me.com>
2021-07-03 02:45:22 +0900

    rehearsal done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/:id', to: 'restaurants#show'
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # post '/restaurants/:id', to: 'restaurants#update'
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+
+100.times do 
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    rating: rand(1..5)
+  )
+end
